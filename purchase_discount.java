4ea2e55bbd9c5be068a6bac5cd1c7c5fc88a00d5
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-09-24 22:41:54 -0300

    Starting Project 2
    -Created package;
    -Import libraries;
    -Start class Main;

---
+++ purchase_discount.java
@@ -0,0 +1,13 @@
+package project2_purchase_discount;
+
+import java.util.*;
+import java.awr.*;
+import javax.swing.*;
+
+public class Main{
+
+	public static void main(String[] args) {
+
+		jTextArea()	
+	}
+}
