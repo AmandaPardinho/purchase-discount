3ca6e666bc9d76e93683bf34ff797ac4b3a086cd
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-09-25 21:37:06 -0300

    Textblock was created

--- purchase_discount.java
+++ purchase_discount.java
@@ -1,13 +1,19 @@
 package project2_purchase_discount;
 
 import java.util.*;
-import java.awr.*;
 import javax.swing.*;
 
 public class Main{
 
 	public static void main(String[] args) {
 
-		//jTextArea()	
+
+		System.out.println("""
+			A bookstore gives a 10% discount to employees and a 5% discount to VIP clients.
+			Calculate the amount to be paid by someone.  
+			The program should read the total amount to be paid and a code identifying 
+			if the client is an employee, a VIP, or an ordinary person.
+		""");
+
 	}
 }
