34c9635db01a41ea7f361ce5c3aba8504ec6cf84
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-09-27 17:48:53 -0300

    Scanner created and user-entered values captured

--- purchase_discount.java
+++ purchase_discount.java
@@ -11,15 +11,23 @@ public class Main{
 		int total = purchaseAmount * discount;
 		int discount;
 		int purchaseAmount;
-
 		String clientString ="";
 
+		Scanner scanner = new Scanner(System.in);
+
 		System.out.println("""
-			A bookstore gives a 10% discount to employees and a 5% discount to VIP cutomer.
+			A bookstore gives a 10% discount to employees and a 5% discount to VIP customer.
 			Calculate the amount to be paid by someone.  
 			The program should read the total amount to be paid and a code identifying 
 			if the client is an employee, a VIP, or an ordinary person.
 			""");
+
+		System.out.println("Enter the purchase amount: ");
+		purchaseAmount = scanner.nextInt();
+
+		System.out.println("Choose a customer code: ");
+		client = scanner.nextInt();
+
 
 		switch (client){
 			case 1:
