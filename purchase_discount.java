c6ebeebf3ecf814690be58e45b548e9d6a09f495
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-09-27 00:04:47 -0300

    Finished switch/case

--- purchase_discount.java
+++ purchase_discount.java
@@ -6,10 +6,16 @@ import javax.swing.*;
 public class Main{
 
 	public static void main(String[] args) {
+		
+		int client;
+		int total = purchaseAmount * discount;
+		int discount;
+		int purchaseAmount;
 
+		String clientString ="";
 
 		System.out.println("""
-			A bookstore gives a 10% discount to employees and a 5% discount to VIP clients.
+			A bookstore gives a 10% discount to employees and a 5% discount to VIP cutomer.
 			Calculate the amount to be paid by someone.  
 			The program should read the total amount to be paid and a code identifying 
 			if the client is an employee, a VIP, or an ordinary person.
@@ -17,12 +23,24 @@ public class Main{
 
 		switch (client){
 			case 1:
-				break;
+				clientString = "employee";
+				discount = 10%;
+				total = purchaseAmount * 0.9;
+				System.out.println(total);
 			case 2:
+				clientString = "vip";
+				discount = 5%;
+				total = purchaseAmount * 0.95;
+				System.out.println(total);
 				break;
 			case 3:
+				clientString = "ordinary customer";
+				discount = 0;
+				total = purchaseAmount;
+				System.out.println(total);
 				break;
 			default:
+				System.out.println("Invalid custumer.");	
 		}
 
 	}
