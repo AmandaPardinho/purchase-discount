9cb6452af23ebd96fa57ea79672d5191d6858354
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-09-29 21:12:38 -0300

    Last commit
    -New variables introduction;
    -Non-essential code has been removed;
    -Reformulated code for calculating total to be paid;
    -Variable corrected;

--- purchase_discount.java
+++ purchase_discount.java
@@ -1,25 +1,26 @@
 package project2_purchase_discount;
 
 import java.util.*;
-import javax.swing.*;
 
 public class Main{
 
 	public static void main(String[] args) {
 		
 		int client;
-		int total = purchaseAmount * discount;
-		int discount;
-		int purchaseAmount;
-		String clientString ="";
-
+		double discountEmploeey = 0.9;
+		double discountVip = 0.95;
+		double discountOrdinary = 1.0;
+		double discount;
+		double purchaseAmount = 0;
+		double total = 0;
+		
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("""
 			A bookstore gives a 10% discount to employees and a 5% discount to VIP customer.
 			Calculate the amount to be paid by someone.  
-			The program should read the total amount to be paid and a code identifying 
-			if the client is an employee, a VIP, or an ordinary person.
+			The program should read the total amount to be paid and a code identifying if the
+			client is an employee, a VIP, or an ordinary person.
 			""");
 
 		System.out.println("Enter the purchase amount: ");
@@ -31,19 +32,17 @@ public class Main{
 
 		switch (client){
 			case 1:
-				clientString = "employee";
-				discount = 10%;
-				total = purchaseAmount * 0.9;
+				discount = discountEmploeey;
+				total = purchaseAmount * discountEmploeey;
 				System.out.println(total);
+				break;
 			case 2:
-				clientString = "vip";
-				discount = 5%;
-				total = purchaseAmount * 0.95;
+				discount = discountVip;
+				total = purchaseAmount * discountVip;
 				System.out.println(total);
 				break;
 			case 3:
-				clientString = "ordinary customer";
-				discount = 0;
+				discount = discountOrdinary;
 				total = purchaseAmount;
 				System.out.println(total);
 				break;
